304874a05fa9b540ed871f632f9860e6c46ced4e
swhyeon98 <smreo75@gmail.com>
2023-11-29 18:00:24 +0900

    feat: 'GameCommand' 입력값 기반 명령어 검색 기능 추가

--- src/main/java/baseball/domain/GameCommand.java
+++ src/main/java/baseball/domain/GameCommand.java
@@ -1,8 +1,10 @@
 package baseball.domain;
 
+import java.util.Arrays;
+
 public enum GameCommand {
     RESTART(1),
-    EXIT(2);
+    QUIT(2);
 
     private final int value;
 
@@ -12,5 +14,13 @@ public enum GameCommand {
 
     public int getValue() {
         return value;
+    }
+
+    public static GameCommand findCommandByInput(int input) {
+        return Arrays.stream(values())
+                .filter(gameCommand -> gameCommand.value == input)
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("[ERROR] 잘못된 입력입니다. " +
+                        "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."));
     }
 }
