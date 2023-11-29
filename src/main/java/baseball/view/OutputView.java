631a578c4fe13178d2cddd09e8600d6c25ddc56f
swhyeon98 <smreo75@gmail.com>
2023-11-29 14:58:59 +0900

    feat: 게임 종료 문구 출력 기능 추가

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -5,4 +5,8 @@ public class OutputView {
     public void printGameStart() {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
+
+    public void printGameOver() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
 }
