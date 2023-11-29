8a4272a2fda2aac3152b90201a3e5eed3f923d4d
swhyeon98 <smreo75@gmail.com>
2023-11-29 17:37:16 +0900

    feat: 스트라이크와 볼 카운트를 관리하는 역할을 'GameStatus' 클래스로 분리

---
+++ src/main/java/baseball/domain/GameStatus.java
@@ -0,0 +1,30 @@
+package baseball.domain;
+
+public class GameStatus {
+    private int strikeCount;
+    private int ballCount;
+
+    public GameStatus() {
+        this.strikeCount = 0;
+        this.ballCount = 0;
+    }
+
+    public void update(int strikeCount, int ballCount) {
+        this.strikeCount = strikeCount;
+        this.ballCount = ballCount;
+    }
+
+    @Override
+    public String toString() {
+        if (strikeCount > 0 && ballCount > 0) {
+            return ballCount + "볼 " + strikeCount + "스트라이크";
+        }
+        if (strikeCount > 0) {
+            return strikeCount + "스트라이크";
+        }
+        if (ballCount > 0) {
+            return ballCount + "볼";
+        }
+        return "낫싱";
+    }
+}
