8ff7d499cc8cecf21843eaafbe67e6f619466ba5
swhyeon98 <smreo75@gmail.com>
2023-11-29 18:41:36 +0900

    refactor: 숫자 검증 로직 개선

--- src/main/java/baseball/controller/BaseballController.java
+++ src/main/java/baseball/controller/BaseballController.java
@@ -1,26 +1,38 @@
 package baseball.controller;
 
+import baseball.service.BaseballService;
 import baseball.view.BaseballView;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class BaseballController {
 
     private static final Pattern NUMBER_PATTERN = Pattern.compile("^[0-9]+$");
 
     private final BaseballView baseballView;
+    private final BaseballService baseballService;
 
-    public BaseballController(BaseballView baseballView) {
+    public BaseballController(BaseballView baseballView, BaseballService baseballService) {
         this.baseballView = baseballView;
+        this.baseballService = baseballService;
     }
 
     public void start() {
+        baseballView.printGameStart();
 
     }
 
-    private void validateIsNumber(String input) {
-        if (!NUMBER_PATTERN.matcher(input).matches()) {
-            throw new IllegalArgumentException("숫자만 입력해주세요.");
+    private List<Integer> validateIsNumber(String input) throws IllegalArgumentException {
+        try {
+            List<Integer> numbers = Arrays.stream(input.split(""))
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList());
+            return numbers;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자만 입력해주세요.");
         }
     }
 }
