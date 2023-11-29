207912557b6bc16fe831c3d6191062fd3fda4ea0
swhyeon98 <smreo75@gmail.com>
2023-11-29 19:30:39 +0900

    feat: Application main 메서드에 게임 시작 로직 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,16 @@
 package baseball;
 
+import baseball.controller.BaseballController;
+import baseball.service.BaseballService;
+import baseball.view.BaseballView;
+import baseball.view.InputView;
+import baseball.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        BaseballView baseballView = new BaseballView(new InputView(), new OutputView());
+        BaseballService baseballService = new BaseballService();
+        BaseballController baseballController = new BaseballController(baseballView, baseballService);
+        baseballController.gameStart();
     }
 }
